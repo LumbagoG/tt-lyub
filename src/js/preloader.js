8ac1b39cfc23487423416ef7dc53a37cfffe2395
Глеб <gleblubiskin@gmail.com>
2022-01-25 17:48:12 +0300

    test

---
+++ src/js/preloader.js
@@ -0,0 +1,9 @@
+//preloader on page start
+window.onload = () => {
+    document.body.classList.add('loaded_hiding')
+
+    window.setTimeout( () => {
+        document.body.classList.add('loaded')
+        document.body.classList.remove('loaded_hiding')
+    }, 1000 )
+}
